f5bf1b302d02c93826d64879015dfe67fdc69040
Jetty.Guo <jetty.guo@kutesmart.com>
2016-03-31 16:16:48 +0800

    学习lua的几个简单demo

---
+++ CreationHelper.lua
@@ -0,0 +1,767 @@
+version = '1.0.3'
+print('script version:', version);
+
+baseURL = "http://source.magicmanufactory.com/creation-crafts/compressed";
+
+__XifuChenyiMap = {
+	["DAA147A"] = "SAN558A",
+	["DAA148A"] = "SAN558A",
+	["DAA149A"] = "SAN558A",
+	["DAA150A"] = "SAQ479A",
+	["DAA151A"] = "SAQ479A",
+	["DAA152A"] = "SAN558A",
+	["DAA032A"] = "SAN558A",
+	["DAA033A"] = "SAQ479A",
+	["DAA034A"] = "SAQ479A",
+	["DAA064A"] = "SAQ479A",
+	["DAA065A"] = "SAN558A",
+	["DAA066A"] = "SAN558A",
+	["DAA067A"] = "SAN558A",
+	["DAA068A"] = "SAN558A",
+	["DAA100A"] = "SAN558A",
+	["DAA146A"] = "SAN558A",
+	["DAA101A"] = "SAN558A",
+	["DAA102A"] = "SAN558A",
+	["DAA103A"] = "SAN558A",
+	["DAA104A"] = "SAN558A",
+	["DAA105A"] = "SAN558A",
+	["DAA106A"] = "SAN558A",
+	["DAA107A"] = "SAN558A",
+	["DAA108A"] = "SAN558A",
+	["DAA059A"] = "SAN558A",
+	["DAA060A"] = "SAN558A",
+	["DAA216A"] = "SAQ479A",
+	["DAA217A"] = "SAN558A",
+	["DAA218A"] = "SAN558A",
+	["DAA219A"] = "SAN558A",
+	["DAA220A"] = "SAQ479A",
+	["DAA229A"] = "SAN558A",
+	["DAA221A"] = "SAN558A",
+	["DAA222A"] = "SAN558A",
+	["DAA223A"] = "SAN558A",
+	["DAA224A"] = "SAQ479A",
+	["DAA225A"] = "SAN558A",
+	["DAA226A"] = "SAN558A",
+	["DAA227A"] = "SAN558A",
+	["DAA228A"] = "SAN558A",
+	["DAA046A"] = "SAQ479A",
+	["DAA045A"] = "SAN558A",
+	["DAA049A"] = "SAQ479A",
+	["DAA050A"] = "SAQ479A",
+	["DAA038A"] = "SAQ479A",
+	["DAA047A"] = "SAQ479A",
+	["DAA048A"] = "SAN558A",
+	["DAA051A"] = "SAN558A",
+	["DAA052A"] = "SAN558A",
+	["DAA040A"] = "SAQ479A",
+	["DAA054A"] = "SAQ479A",
+	["DAA055A"] = "SAQ479A",
+	["DAA056A"] = "SAN558A",
+	["DAA057A"] = "SAL046A",
+	["DAA058A"] = "SAN558A",
+	["DAA061A"] = "SAN558A",
+	["DAA062A"] = "SAN558A",
+	["DAA063A"] = "SAN558A",
+	["DAP953A"] = "SAN558A",
+	["DAP957A"] = "SAN558A",
+	["DAQ1864"] = "SAQ497A",
+	["DAQ1865"] = "SAN558A",
+	["DAQ1870"] = "SAN558A",
+	["DAQ1871"] = "SAQ110A",
+	["DAQ1872"] = "SAQ479A",
+	["DAQ1873"] = "SAN558A",
+	["DAQ1874"] = "SAN558A",
+	["DAQ1875"] = "SAN558A",
+	["DAQ1876"] = "SAQ479A",
+	["DAQ1877"] = "SAN558A",
+	["DAQ1878"] = "SAN558A",
+	["DAQ1879"] = "SAN558A",
+	["DAQ1880"] = "SAQ479A",
+	["DAQ1939"] = "SAN558A",
+	["DAQ1940"] = "SAQ479A",
+	["DAQ1941"] = "SAQ479A",
+	["DAQ1942"] = "SAN558A",
+	["DAQ1951"] = "SAQ107A",
+	["DAQ1955"] = "SAQ107A",
+	["DAQ1956"] = "SAN558A",
+	["DAQ2853"] = "SAN558A",
+	["DAQ2861"] = "SAQ479A",
+	["DAR002A"] = "SAL046A",
+	["DAR004A"] = "SAN558A",
+	["DAR005A"] = "SAL046A",
+	["DAR006A"] = "SAL046A",
+	["DAR007A"] = "SAN545A",
+	["DAR008A"] = "SAN558A",
+	["DAR009A"] = "SAN558A",
+	["DAR010A"] = "SAN558A",
+	["DAR012A"] = "SAN558A",
+	["DAR016A"] = "SAN558A",
+	["DAR018A"] = "SAN558A",
+	["DAR021A"] = "SAQ479A",
+};
+
+__MajiaChenyiMap = {
+	["DAP953A"] = "SAN558A",
+	["DAP957A"] = "SAN558A",
+	["DAQ1864"] = "SAQ497A",
+	["DAQ1865"] = "SAN558A",
+	["DAQ1870"] = "SAN558A",
+	["DAQ1871"] = "SAQ110A",
+	["DAQ1872"] = "SAQ479A",
+	["DAQ1873"] = "SAN558A",
+	["DAQ1874"] = "SAN558A",
+	["DAQ1875"] = "SAN558A",
+	["DAQ1876"] = "SAQ479A",
+	["DAQ1877"] = "SAN558A",
+	["DAQ1878"] = "SAN558A",
+	["DAQ1879"] = "SAN558A",
+	["DAQ1880"] = "SAQ479A",
+	["DAQ1939"] = "SAN558A",
+	["DAQ1940"] = "SAQ479A",
+	["DAQ1941"] = "SAQ479A",
+	["DAQ1942"] = "SAN558A",
+	["DAQ1951"] = "SAQ107A",
+	["DAQ1955"] = "SAQ107A",
+	["DAQ1956"] = "SAN558A",
+	["DAQ2853"] = "SAN558A",
+	["DAQ2861"] = "SAQ479A",
+	["DAR002A"] = "SAL046A",
+	["DAR004A"] = "SAN558A",
+	["DAR005A"] = "SAL046A",
+	["DAR006A"] = "SAL046A",
+	["DAR007A"] = "SAN545A",
+	["DAR008A"] = "SAN558A",
+	["DAR009A"] = "SAN558A",
+	["DAR010A"] = "SAN558A",
+	["DAR012A"] = "SAN558A",
+	["DAR016A"] = "SAN558A",
+	["DAR018A"] = "SAN558A",
+	["DAR021A"] = "SAQ479A",
+	["DAA046A"] = "SAQ479A",
+	["DAA045A"] = "SAN558A",
+	["DAA049A"] = "SAQ479A",
+	["DAA050A"] = "SAQ479A",
+	["DAA038A"] = "SAQ479A",
+	["DAA047A"] = "SAQ479A",
+	["DAA048A"] = "SAN558A",
+	["DAA051A"] = "SAN558A",
+	["DAA052A"] = "SAN558A",
+	["DAA040A"] = "SAQ479A",
+	["DAA054A"] = "SAQ479A",
+	["DAA055A"] = "SAQ479A",
+	["DAA056A"] = "SAN558A",
+	["DAA057A"] = "SAL046A",
+	["DAA058A"] = "SAN558A",
+	["DAA061A"] = "SAN558A",
+	["DAA062A"] = "SAN558A",
+	["DAA063A"] = "SAN558A",
+	["DAA059A"] = "SAN558A",
+	["DAA060A"] = "SAN558A",
+	["DAA216A"] = "SAQ479A",
+	["DAA217A"] = "SAN558A",
+	["DAA218A"] = "SAN558A",
+	["DAA219A"] = "SAN558A",
+	["DAA220A"] = "SAQ479A",
+	["DAA229A"] = "SAN558A",
+	["DAA221A"] = "SAN558A",
+	["DAA222A"] = "SAN558A",
+	["DAA223A"] = "SAN558A",
+	["DAA224A"] = "SAQ479A",
+	["DAA225A"] = "SAN558A",
+	["DAA226A"] = "SAN558A",
+	["DAA227A"] = "SAN558A",
+	["DAA228A"] = "SAN558A",
+};
+
+__DayiChenyiMap = {
+	["DAA027A"] = "SAK066A",
+	["DAA028A"] = "SAK066A",
+	["DAA154A"] = "SAK066A",
+	["DAA155A"] = "SAK066A",
+	["DAA156A"] = "SAM049A",
+	["DAA157A"] = "SAK066A",
+	["DAA158A"] = "SAK066A",
+	["DAA159A"] = "SAM049A",
+	["DAA160A"] = "SAK066A",
+	["DAA161A"] = "SAK066A",
+	["DAA162A"] = "SAK066A",
+	["DAA163A"] = "SAK066A",
+	["DAA164A"] = "SAK066A",
+	["DAA165A"] = "SAK066A",
+	["DAA166A"] = "SAK066A",
+};
+
+__XifuLingdaiMap = {
+	["DAA147A"] = "03",
+	["DAA148A"] = "06",
+	["DAA149A"] = "06",
+	["DAA150A"] = "03",
+	["DAA151A"] = "03",
+	["DAA152A"] = "08",
+	["DAA032A"] = "06",
+	["DAA033A"] = "03",
+	["DAA034A"] = "06",
+	["DAA064A"] = "06",
+	["DAA065A"] = "08",
+	["DAA066A"] = "11",
+	["DAA067A"] = "11",
+	["DAA068A"] = "11",
+	["DAA100A"] = "11",
+	["DAA146A"] = "11",
+	["DAA101A"] = "08",
+	["DAA102A"] = "06",
+	["DAA103A"] = "09",
+	["DAA104A"] = "11",
+	["DAA105A"] = "06",
+	["DAA106A"] = "03",
+	["DAA107A"] = "06",
+	["DAA108A"] = "06",
+	["DAA059A"] = "11",
+	["DAA060A"] = "11",
+	["DAA216A"] = "06",
+	["DAA217A"] = "01",
+	["DAA218A"] = "11",
+	["DAA219A"] = "07",
+	["DAA220A"] = "07",
+	["DAA229A"] = "11",
+	["DAA221A"] = "09",
+	["DAA222A"] = "01",
+	["DAA223A"] = "02",
+	["DAA224A"] = "11",
+	["DAA225A"] = "11",
+	["DAA226A"] = "07",
+	["DAA227A"] = "07",
+	["DAA228A"] = "07",
+	["DAA046A"] = "11",
+	["DAA045A"] = "10",
+	["DAA049A"] = "11",
+	["DAA050A"] = "11",
+	["DAA038A"] = "11",
+	["DAA047A"] = "11",
+	["DAA048A"] = "06",
+	["DAA051A"] = "06",
+	["DAA052A"] = "06",
+	["DAA040A"] = "06",
+	["DAA054A"] = "06",
+	["DAA055A"] = "11",
+	["DAA056A"] = "10",
+	["DAA057A"] = "01",
+	["DAA058A"] = "11",
+	["DAA061A"] = "11",
+	["DAA062A"] = "09",
+	["DAA063A"] = "06",
+	["DAP953A"] = "10",
+	["DAP957A"] = "01",
+	["DAQ1864"] = "06",
+	["DAQ1865"] = "08",
+	["DAQ1870"] = "01",
+	["DAQ1871"] = "09",
+	["DAQ1872"] = "10",
+	["DAQ1873"] = "11",
+	["DAQ1874"] = "11",
+	["DAQ1875"] = "11",
+	["DAQ1876"] = "11",
+	["DAQ1877"] = "06",
+	["DAQ1878"] = "11",
+	["DAQ1879"] = "01",
+	["DAQ1880"] = "06",
+	["DAQ1939"] = "06",
+	["DAQ1940"] = "07",
+	["DAQ1941"] = "07",
+	["DAQ1942"] = "03",
+	["DAQ1951"] = "07",
+	["DAQ1955"] = "10",
+	["DAQ1956"] = "11",
+	["DAQ2853"] = "09",
+	["DAQ2861"] = "09",
+	["DAR002A"] = "11",
+	["DAR004A"] = "11",
+	["DAR005A"] = "11",
+	["DAR006A"] = "11",
+	["DAR007A"] = "11",
+	["DAR008A"] = "03",
+	["DAR009A"] = "11",
+	["DAR010A"] = "03",
+	["DAR012A"] = "03",
+	["DAR016A"] = "03",
+	["DAR018A"] = "01",
+	["DAR021A"] = "11",
+};
+
+function getXifuImageFAndC(direction, material, design, crafts)
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/xifu/' .. material .. '/' .. direction ..'/';
+	--衬衣
+	local chenyi = __XifuChenyiMap[material];
+	if (chenyi) then
+		result[i] = baseURL .. '/xifu/chenyi/'.. chenyi  ..'/'.. design ..'/' .. direction .. '.png';
+		i = i + 1;
+	end
+	if (design == '0023') then
+		-- do nothing
+	elseif (design == '0027') then
+		-- do nothing
+	else
+		--领带
+		local lingdai = __XifuLingdaiMap[material];
+		if (lingdai) then
+			result[i] = baseURL .. '/xifu/lingdai/' .. direction ..'/'.. lingdai ..'.png';
+			i = i + 1;
+		end
+	end
+	--大身
+	result[i] = prefix .. design .. '.png';
+	i = i + 1;
+	--胸口袋 17
+	local xiongkoudai = crafts['17'];
+	if ( xiongkoudai ~= '0000' ) then
+		result[i] = prefix .. xiongkoudai .. '-' .. design .. '.png';
+		i = i + 1;
+	end
+	local botou = crafts['15'];
+	if ( botou ~= '0000' ) then
+		if (design == '0023' ) then
+			-- do nothing
+		elseif (design == '0027') then
+			--do nothing
+		else
+			result[i] = prefix .. botou .. '-' .. design .. '.png';
+			i = i + 1;
+		end
+	end
+	local xiakoudai = crafts['18'];
+	if ( xiakoudai ~= '0000' ) then
+		result[i] = prefix .. xiakoudai .. '-' ..design .. '.png';
+		i = i + 1;
+	end
+	local jianpan = crafts['19'];
+	if ( jianpan ~= '0000' ) then
+		result[i] = prefix .. jianpan .. '.png';
+		i = i + 1;
+	end
+	local xiukou = crafts['20']; 
+	if ( xiukou ~= '0000' ) then
+		result[i] = prefix .. xiukou .. '.png';
+		i = i + 1;
+	end
+	return result;
+end
+
+function getXifuImageF( material, design, crafts )
+	return getXifuImageFAndC( 'F', material, design, crafts);
+end
+
+function getXifuImageC( material, design, crafts )
+	return getXifuImageFAndC( 'C', material, design, crafts);
+end
+
+function getXifuImageB( material, design, crafts )
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/xifu/' .. material .. '/B/';
+	--衬衣
+	local chenyi = __XifuChenyiMap[material];
+	if (chenyi) then
+		result[i] = baseURL .. '/xifu/chenyi/'.. chenyi  ..'/'.. design ..'/B.png';
+		i = i + 1;
+	end
+	--后片
+	local houpian = crafts['16'];
+	if ( houpian ) then
+		result[i] = prefix .. houpian .. '.png';
+		i = i + 1;
+	end
+	local botou = crafts['15'];
+	if ( botou ~= '0000' ) then
+		if (design == '0023' ) then
+			botou = '0009';
+		elseif (design == '0027') then
+			botou = '0009';
+		else
+			botou = '0001';
+		end
+		result[i] = prefix .. botou .. '.png';
+		i = i + 1;
+	end
+	local xiukou = crafts['20']; 
+	if ( xiukou ) then
+		if ( xiukou == '0000' ) then
+			xiukou = '0621';
+		end
+		result[i] = prefix .. xiukou .. '.png';
+		i = i + 1;
+	end
+	local zhoudian = crafts['21']; 
+	if ( zhoudian ~= '0000' ) then
+		result[i] = prefix .. zhoudian .. '.png';
+		i = i + 1;
+	end
+	return result;
+end
+
+function getChenyiImageFAndC(direction, material, design, crafts)
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/chenyi/' .. material .. '/' .. direction ..'/';
+	--下摆
+	local xiabai = crafts['11'];
+	if ( xiabai ~= '0000' ) then
+		result[i] = prefix .. xiabai .. '.png';
+		i = i + 1;
+	end
+	--袖口  13短袖 或者 14长袖
+	local changxiu = crafts['14'];
+	if ( changxiu ~= '0000' ) then
+		result[i] = prefix .. changxiu .. '-' .. xiabai .. '.png';
+		i = i + 1;
+	else
+		local duanxiu = crafts['13'];
+		if ( duanxiu ~= '0000' ) then
+			result[i] = prefix .. duanxiu .. '.png';
+			i = i + 1;
+		end
+	end
+	local botou = crafts['9'];
+	if ( botou ~= '0000' ) then
+		result[i] = prefix .. botou .. '.png';
+		i = i + 1;
+	end
+	local menji = design;
+	if ( menji ~= '0000' ) then
+		result[i] = prefix .. menji  .. '.png';
+		i = i + 1;
+	end
+	local qianpian = crafts['10'];
+	if ( qianpian ~= '0000' ) then
+		result[i] = prefix .. qianpian .. '.png';
+		i = i + 1;
+	end
+	local xiongdai = crafts['12']; 
+	if ( xiongdai ~= '0000' ) then
+		result[i] = prefix .. xiongdai .. '.png';
+		i = i + 1;
+	end
+	return result;
+end
+
+function getChenyiImageF( material, design, crafts )
+	return getChenyiImageFAndC('F', material, design, crafts);
+end
+function getChenyiImageC( material, design, crafts )
+	return getChenyiImageFAndC('C', material, design, crafts);
+end
+
+--背身
+function getChenyiImageB( material, design, crafts )
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/chenyi/' .. material .. '/B/';
+	--下摆
+	local xiabai = crafts['11'];
+	--(下摆)后背
+	local houbei = crafts['8'];
+	if ( houbei ~= '0000' ) then
+		if (xiabai ~= '0000') then
+			result[i] = prefix .. xiabai .. '_' .. houbei .. '.png';
+			i = i + 1;
+		else
+			result[i] = prefix .. houbei .. '.png';
+			i = i + 1;
+		end
+	end
+	local botou = crafts['9'];
+	if ( botou ~= '0000' ) then
+		result[i] = prefix .. botou .. '.png';
+		i = i + 1;
+	end
+	--袖口  13短袖 或者 14长袖
+	local changxiu = crafts['14'];
+	if ( changxiu ~= '0000' ) then
+		result[i] = prefix .. changxiu .. '-' .. xiabai .. '.png';
+		i = i + 1;
+	else
+		local duanxiu = crafts['13'];
+		if ( duanxiu ~= '0000' ) then
+			result[i] = prefix .. duanxiu .. '.png';
+			i = i + 1;
+		end
+	end
+	return result;
+end
+
+function getDayiImageFAndC(direction, material, design, crafts)
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/dayi/' .. material .. '/' .. direction ..'/';
+	--衬衣
+	local chenyi = __DayiChenyiMap[material];
+	if (chenyi) then
+		result[i] = baseURL .. '/dayi/chenyi/'.. chenyi  ..'/' .. direction .. '.png';
+		i = i + 1;
+	end
+	--大身
+	result[i] = prefix .. design .. '.png';
+	i = i + 1;
+	--胸口袋 17
+	local xiongkoudai = crafts['24'];
+	if ( xiongkoudai ~= '0000' ) then
+		result[i] = prefix .. xiongkoudai .. '.png';
+		i = i + 1;
+	end
+	local botou = crafts['22'];
+	if ( botou ~= '0000' ) then
+		result[i] = prefix .. botou .. '-' .. design .. '.png';
+		i = i + 1;
+	end
+	local xiakoudai = crafts['25'];
+	if ( xiakoudai ~= '0000' ) then
+		result[i] = prefix .. xiakoudai .. '.png';
+		i = i + 1;
+	end
+	local jianpan = crafts['26'];
+	if ( jianpan ~= '0000' ) then
+		result[i] = prefix .. jianpan .. '.png';
+		i = i + 1;
+	end
+	local xiukou = crafts['27']; 
+	if ( xiukou ~= '0000' ) then
+		result[i] = prefix .. xiukou .. '.png';
+		i = i + 1;
+	end
+	return result;
+end
+
+function getDayiImageF( material, design, crafts )
+	return getDayiImageFAndC( 'F', material, design, crafts);
+end
+
+function getDayiImageC( material, design, crafts )
+	return getDayiImageFAndC( 'C', material, design, crafts);
+end
+
+function getMajiaImageFAndC(direction, material, design, crafts)
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/majia/' .. material .. '/' .. direction .. '/';
+	--衬衣
+	local chenyi = __MajiaChenyiMap[material];
+	if (chenyi) then
+		result[i] = baseURL .. '/majia/chenyi/'.. chenyi  ..'/' .. direction .. '_' .. chenyi .. '.png';
+		i = i + 1;
+	end
+	--下摆
+	local xiabai = crafts['2'];
+	--驳头
+	local botou = crafts['1'];
+	--大身
+	local dashen = design .. '_' .. xiabai .. '_' .. botou;
+	result[i] = prefix .. dashen .. '.png';
+	i = i + 1;
+	--领子（驳头）
+	if ( botou ~= '0000' ) then
+		result[i] = prefix .. botou .. '-' .. design .. '.png';
+		i = i + 1;
+	end
+	local xiakoudai = crafts['3'];
+	local xiongdai = crafts['4'];
+	result[i] = prefix .. xiakoudai .. '.png';
+	i = i + 1;
+	result[i] = prefix .. xiongdai .. '.png';
+	i = i + 1;
+	return result;
+end
+
+function getMajiaImageF( material, design, crafts )
+	return getMajiaImageFAndC('F', material, design, crafts);
+end
+function getMajiaImageC( material, design, crafts )
+	return getMajiaImageFAndC('C', material, design, crafts);
+end
+
+--背身
+function getMajiaImageB( material, design, crafts )
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/majia/' .. material .. '/B/';
+	--衬衣
+	local chenyi = __MajiaChenyiMap[material];
+	if (chenyi) then
+		result[i] = baseURL .. '/majia/chenyi/'.. chenyi  ..'/' .. 'B_' .. chenyi .. '.png';
+		i = i + 1;
+	end
+	--后片
+	result[i] = prefix .. '421C.png';
+	i = i + 1;
+	--领子（驳头）
+	local botou = crafts['1'];
+	if ( botou ~= '0000' ) then
+		result[i] = prefix .. botou .. '-' .. design .. '.png';
+		i = i + 1;
+	end
+	return result;
+end
+
+function getXikuImageF(material, design, crafts)
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/xiku/' .. material .. '/F/';
+	--裤型
+	local kuxing = design;
+	local qiankoudai = crafts['7'];
+	--裤口
+	local kukou = crafts['6'];
+	result[i] = prefix .. kuxing .. '.png';
+	i = i + 1;
+	result[i] = prefix .. qiankoudai .. '.png';
+	i = i + 1;
+	result[i] = prefix .. kukou .. '.png';
+	i = i + 1;
+	return result;
+end
+
+function getXikuImageC( material, design, crafts )
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/xiku/' .. material .. '/C/';
+	--裤型
+	local kuxing = design;
+	--前口袋
+	local qiankoudai = crafts['7'];
+	if (qiankoudai == '3104') then
+		qiankoudai = design .. '_3104';
+	end
+	--裤口
+	local kukou = crafts['6'];
+
+	result[i] = prefix .. kuxing .. '.png';
+	i = i + 1;
+	result[i] = prefix .. qiankoudai .. '.png';
+	i = i + 1;
+	result[i] = prefix .. kukou .. '.png';
+	i = i + 1;
+	return result;
+end
+
+--背身
+function getXikuImageB( material, design, crafts )
+	local result = {};
+	local i = 1;
+	local prefix = baseURL .. '/xiku/' .. material .. '/B/';
+	--裤型
+	local kuxing = 'base';
+	--后口袋
+	local houkoudai = crafts['5'];
+	--裤口
+	local kukou = crafts['6'];
+
+	result[i] = prefix .. kuxing .. '.png';
+	i = i + 1;
+	result[i] = prefix .. houkoudai .. '.png';
+	i = i + 1;
+	result[i] = prefix .. kukou .. '.png';
+	i = i + 1;
+	return result;
+end
+
+
+-- require "GlobalConstant"
+-- require "LingdaiMap"
+-- require "ChenyiMap"
+-- require "XifuImageURLHelper"
+
+function getCodeFormItems(items)
+	local result = {};
+	for k,v in pairs(items) do
+		result[k] = v['code'];
+	end
+	return result;
+end
+
+route = {
+	['西装'] = {
+		['F'] = getXifuImageF,
+		['C'] = getXifuImageC,
+		['B'] = getXifuImageB,
+	},
+	['大衣'] = {
+		['F'] = getDayiImageF,
+		['C'] = getDayiImageC,
+	},
+	['衬衣'] = {
+		['F'] = getChenyiImageF,
+		['C'] = getChenyiImageC,
+		['B'] = getChenyiImageB,
+	},
+	['马甲'] = {
+		['F'] = getMajiaImageF,
+		['C'] = getMajiaImageC,
+		['B'] = getMajiaImageB,
+	},
+	['裤子'] = {
+		['F'] = getXikuImageF,
+		['C'] = getXikuImageC,
+		['B'] = getXikuImageB,
+	},
+}
+
+function pairsByKeys(t)  
+    local a = {}  
+    for n in pairs(t) do  
+        a[#a+1] = n  
+    end  
+    table.sort(a)  
+    local i = 0  
+    return function()  
+        i = i + 1  
+        return a[i], t[a[i]]  
+    end  
+end
+
+function getImageIdentify(category, direction, material, design, crafts)
+	local identify = 'composite/imgs/' .. category .. '/' .. direction .. '/' .. material .. '/design-' .. design;
+	local code = getCodeFormItems(crafts);
+	for k,v in pairsByKeys(code) do
+		identify = identify .. '_' .. k .. '-' .. v;
+	end
+	return identify;
+end
+
+-- routeIdentify = {
+-- 	['西装'] = getXifuIdentify,
+-- }
+
+function getImageURL( category, direction, material, design, crafts )
+	local code = getCodeFormItems(crafts);
+	local result = route[category][direction](material, design, code);
+	return result;
+end
+
+-- args = {
+-- 	['15'] = {
+-- 		['code'] = '0001';
+-- 	},
+-- 	['16'] = {
+-- 		['code'] = '04N1';
+-- 	},
+-- 	['17'] =  {
+-- 		['code'] = '0101';
+-- 	},
+-- 	['18'] = {
+-- 		['code'] = '0261';
+-- 	},
+-- 	['19'] = {
+-- 		['code'] = '0000';
+-- 	},
+-- 	['20'] = {
+-- 		['code'] = '0000';
+-- 	},
+-- 	['21'] = {
+-- 		['code'] = '0000';
+-- 	},
+-- }
+-- result = getImageURL('西装', 'C', 'DAA032A', '0012', args);
+-- -- result = getCodeFormItems(args);
+-- for i,v in ipairs(result) do
+-- 	print(i,v)
+-- end
---
+++ hello.lua
@@ -0,0 +1,254 @@
+print "asdas"
+
+
+a = "hello" .. "world"   .."郭帅永远是冠军"
+
+print (a)
+
+site = {}
+site["key"] = "www.baidu.com"
+-- lua 赋值的基本方式
+print (site["key"])
+--lua 变量
+print (site.key)
+
+
+
+
+a,b,c = 0,1
+print(a,b,c)
+x, y = 1,2
+print (x,y)
+
+x, y = y, x
+
+
+
+print (x,y)
+a, b, c = 0
+
+print(a,b,c)
+
+-- 循环
+--[[  多行注释
+while( true )
+do
+   print("循环将永远执行下去")
+end
+]]
+
+if(0)   --0为true  因为true英文就是确定的意思，通常1表示肯定，而0表示否定即false，false的错误否定的意思，所以就将1定义为true，0定义为flase，这个是bool变量的约定，布尔是这样设定的
+
+then
+	print ("0为true")
+end
+
+a = 10
+if (a < 20)
+then
+	print ("a小于20")
+
+end
+
+
+--[定义变量]
+a = 100
+if(a<20)
+then
+	print ("a小于20")
+
+else
+	print("a > 20")
+end
+
+
+--[ 定义变量 --]
+a = 100
+
+--[ 检查布尔条件 --]
+if( a == 10 )
+then
+   --[ 如果条件为 true 打印以下信息 --]
+   print("a 的值为 10" )
+elseif( a == 20 )
+then
+   --[ if else if 条件为 true 时打印以下信息 --]
+   print("a 的值为 20" )
+elseif( a == 30 )
+then
+   --[ if else if condition 条件为 true 时打印以下信息 --]
+   print("a 的值为 30" )
+else
+   --[ 以上条件语句没有一个为 true 时打印以下信息 --]
+   print("没有匹配 a 的值" )
+end
+print("a 的真实值为: ", a )
+
+
+
+--[ 定义变量 --]
+a = 100;
+b = 200;
+
+if(a == 100)
+then
+	if( b == 200)
+	then
+	      print("a 的值为 100 b 的值为 200" );
+		end
+end
+
+
+--[[ 函数返回两个值的最大值 --]]
+function max(num1, num2)
+
+   if (num1 > num2) then
+      result = num1;
+   else
+      result = num2;
+   end
+
+   return result;
+end
+
+
+
+print("两值比较最大值为 ",max(10,4))
+print("两值比较最大值为 ",max(5,6))
+
+
+
+--Lua 中我们可以将函数作为参数传递给函数，如下实例：
+
+myprint = function(param)
+	print ("这是打印函数- ##"..param.."##")
+
+end
+
+function add(num1,num2,functionPrint)
+result = num1 + num2
+functionPrint(result)
+end
+
+
+myprint(10)
+
+add(2,4,myprint)
+
+-- Lua函数可以返回多个结果值，比如string.find，其返回匹配串"开始和结束的下标"（如果不存在匹配串返回nil）。
+s,e =  string.find("w3cschool菜鸟教程：www.w3cschool.cc", "菜鸟教程")
+
+print (s,e)
+
+
+a = true
+b = true
+
+if ( a and b )
+then
+   print("a and b - 条件为 true" )
+end
+
+if ( a or b )
+then
+   print("a or b - 条件为 true" )
+end
+
+print("---------分割线---------" )
+
+-- 修改 a 和 b 的值
+a = false
+b = true
+
+if ( a and b )
+then
+   print("a and b - 条件为 true" )
+else
+   print("a and b - 条件为 false" )
+end
+
+if ( not( a and b) )
+then
+   print("not( a and b) - 条件为 true" )
+else
+   print("not( a and b) - 条件为 false" )
+end
+
+a = "Hello "
+b = "World"
+
+
+print("连接字符串 a 和 b ", a..b )
+
+print("b 字符串长度 ",#b )
+print("字符串 Test 长度 ",#"Test" )
+print("w3cschool菜鸟教程网址长度 ",#"www.w3cschool.cc" )
+
+a = 20
+b = 10
+c = 15
+d = 5
+
+e = (a + b) * c / d; -- ( 30 * 15 ) / 5
+print (e)
+
+string1 = "Lua"
+print("\"字符串 1 是\"",string1)
+string2 = 'w3cschool.cc'
+print("字符串 2 是",string2)
+string3 = [["Lua 教程"]]
+print("字符串 3 是",string3)
+
+string = "Lua Tutorial"
+
+print(string.find(string,"T"))
+reversedString = string.reverse(string)
+print("新字符串为",reversedString)
+
+string1 = "Lua"
+string2 = "Tutorial"
+number1 = 10
+number2 = 20
+-- 基本字符串格式化
+print(string.format("基本格式化 %s %s",string1,string2))
+-- 日期格式化
+date = 2; month = 1; year = 2014
+print(string.format("日期格式化 %02d/%02d/%03d", date, month, year))
+-- 十进制格式化
+print(string.format("%.4f",1/3))
+
+
+
+-- 字符转换
+-- 转换第一个字符
+print(string.byte("Lua"))
+-- 转换第三个字符
+print(string.byte("Lua",3))
+-- 转换末尾第一个字符
+print(string.byte("Lua",-1))
+-- 第二个字符
+print(string.byte("Lua",2))
+-- 转换末尾第二个字符
+print(string.byte("Lua",-2))
+
+-- 整数 ASCII 码转换为字符
+print(string.char(97))
+
+
+
+string1 = "www."
+string2 = "w3cschool"
+string3 = ".cc"
+-- 使用 .. 进行字符串连接
+print("连接字符串",string1..string2..string3)
+
+-- 字符串长度
+print("字符串长度 ",string.len(string2))
+
+-- 字符串复制 2 次
+repeatedString = string.rep(string2,2)
+print(repeatedString)
+
+
+array = {"Lua", "Tutorial"}
+
